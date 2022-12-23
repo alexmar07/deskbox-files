3e51dbd4305ef223fe5edac7ba369b5d6f665f28
Alessandro Marotta <alessand.marotta@gmail.com>
2022-12-23 11:04:35 +0100

    Configurazione app provider per le dipendenze di repository e servizi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,46 @@
 
 namespace App\Providers;
 
+use App\Models\File;
+use App\Http\Services\FileService;
 use Illuminate\Support\ServiceProvider;
+use App\Http\Repositories\FileRepository;
+use App\Http\Contracts\Services\FileServiceContract;
+use App\Http\Contracts\Repositories\FileRepositoryContract;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+
+    protected $repositories = [
+        FileRepositoryContract::class => [
+            'concrete' => FileRepository::class,
+            'model' => File::class,
+        ]
+    ];
+
+    protected $services = [
+        FileServiceContract::class => [
+            'repository'    => FileRepositoryContract::class,
+            'service'       => FileService::class
+        ]
+    ];
+
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register() {
+
+        foreach($this->repositories as $abstract => $impl) {
+            app()->bind($abstract, function ($app) use ($impl) {
+                return new $impl['concrete'](new $impl['model']);
+            });
+        }
+
+        foreach ($this->services as $abstract => $impl) {
+            app()->bind($abstract, function ($app) use ($impl) {
+                return new $impl['service']($app->make($impl['repository']));
+            });
+        }
     }
 }
