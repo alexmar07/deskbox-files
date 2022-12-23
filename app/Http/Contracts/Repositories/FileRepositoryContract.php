668367d629102c0de6f8b26450be448b2fe661cc
Alessandro Marotta <alessand.marotta@gmail.com>
2022-12-23 11:00:54 +0100

    Inizializzazione contratti

---
+++ app/Http/Contracts/Repositories/FileRepositoryContract.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Contracts\Repositories;
+
+/**
+ * Interfaccia per la definizione delle funzioni
+ * del repository di un file
+ */
+interface FileRepositoryContract {
+
+}
---
+++ app/Http/Contracts/Services/FileServiceContract.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Contracts\Services;
+
+/**
+ * Interfaccia per la definizione delle funzioni del servizio
+ * per i file
+ */
+interface FileServiceContract {
+
+}
